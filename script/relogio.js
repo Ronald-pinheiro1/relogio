87deebd4e4f32b7b8e2d0b26c693ea7ce46781a9
Ronald Pinheiro <pinheiroronald12@gmail.com>
2024-09-16 20:38:37 -0300

    atu

--- script/relogio.js
+++ script/relogio.js
@@ -16,32 +16,40 @@ function tempo(){
     let h = rel.getHours();
     let m = rel.getMinutes();
     let s = rel.getSeconds();
-    res.innerHTML=''
     
-    if(h <= 9) h= num[h];
-    if(m <= 9) m= num[m];
-    if(s <= 9) s= num[s];
+    
+    h = h.toString().padStart(2, '0');
+    m = m.toString().padStart(2, '0');
+    s = s.toString().padStart(2, '0');
  
-    res.innerHTML+=`<span>${h}</span>:<span>${m}</span>:<span>${s}</span>`;
-
+    //Contador (relógio)
+    res.innerHTML=`<span>${h}</span>:<span>${m}</span>:<span>${s}</span>`;
+    
+    //contador por extenço
     alarme.innerHTML = `${N60[rel.getHours()]} horas, ${N60[rel.getMinutes()]} minutos e  ${N60[rel.getSeconds()]} segundos`;
 }
-//saldação e calendário doa dia
-(function(){
+
+//Chamando a função tempo a cada segundo
+setInterval(tempo, 1000)
+
+//funçao para atualizar saudação e calendário
+function atualisaldcaoedata(){
     let rel = new Date();
     let h = rel.getHours();
     let saldacao = 'Bom dia, Visitante! Vamos dar uma olhadinha no tempo?';
     
-    if(h >= 0 && h <= 4) saldacao = 'É de madrugada visitante, vamos dormir!';
-    
-    if(h >= 12)saldacao = "Boa tarde, Visitante! Vamos dar uma olhadinha no tempo?";
-    
-    if(h >=18)saldacao = 'Boa noite, Visitante! Vamos dar uma olhadinha no tempo?';
+    if(h >= 0 && h < 5) saldacao = 'É de madrugada visitante, vamos dormir!';
 
+    else if(h >= 12 && h < 18)saldacao = "Boa tarde, Visitante! Vamos dar uma olhadinha no tempo?";
+    
+    else if(h >=18)saldacao = 'Boa noite, Visitante! Vamos dar uma olhadinha no tempo?';
 
+    //Saldação do dia
     sal.innerHTML = `<strong>${saldacao}</strong>`;
-
+    //Calendário(semana, mês, dia e ano)
     data.innerHTML=`<i>Hoje é ${semana[rel.getDay()]}, ${rel.getDate()} ${meses[rel.getMonth()]} de ${rel.getFullYear()}</i>`;
-})();
+};
+atualisaldcaoedata()
+setInterval(atualisaldcaoedata, 60000);
 
 
